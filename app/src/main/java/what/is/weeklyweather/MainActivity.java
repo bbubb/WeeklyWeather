8db24a5e2f0ca591a80dd53776b0d3df24e3ac8f
bbubb <brandoncbunch@gmail.com>
2019-07-19 11:57:51 -0400

    Base layout and code done TODO: import icons adapter/listview

--- app/src/main/java/what/is/weeklyweather/MainActivity.java
+++ app/src/main/java/what/is/weeklyweather/MainActivity.java
@@ -1,16 +1,30 @@
 package what.is.weeklyweather;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    CurrentWeatherFrag currentWeatherFrag;
+    FiveDayForecastFrag fiveDayForecastFrag;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        currentWeatherFrag = new CurrentWeatherFrag();
+        loadFragment(R.id.frame_current_forecast, currentWeatherFrag, "Current Weather");
+        fiveDayForecastFrag = new FiveDayForecastFrag();
+        loadFragment(R.id.frame_5day_forecast, fiveDayForecastFrag, "5 Day Forecast");
+
+    }
+
+    private void loadFragment(int frameLayout, Fragment fragment, String tag){
+        getSupportFragmentManager().beginTransaction().add(frameLayout, fragment, tag).commit();
     }
 }
